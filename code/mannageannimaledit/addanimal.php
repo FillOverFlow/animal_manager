4ca75245241b34cc8fe397e5c4981be29614c4e0
Tinnakron <sb5840248117@lru.ac.th>
2020-06-18 16:45:15 +0700

    showlistanimal

---
+++ code/mannageannimaledit/addanimal.php
@@ -0,0 +1,6 @@
+<?php
+foreach ($_POST['Animal_ID'] as  $value) {
+print_r($value);
+}
+var_dump($_POST);
+ ?>
--- mannageannimaledit.php
+++ mannageannimaledit.php
@@ -1,3 +1,11 @@
+<?php
+require_once('code/mannageannimaledit/class.php');
+$listanimal = listanimal();
+// echo "<pre>";
+// var_dump($listanimal);
+// echo "</pre>";
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -30,199 +38,206 @@
   <?php include('menu.php');  ?>
 
 
-<div id="display-2" class="col-9 p-5 border border-dark rounded h-80 w-100"> 
+  <div id="display-2" class="col-9 p-5 border border-dark rounded h-80 w-100"> 
 
-  <div class="row mt-5">
-    <div class="col-1"></div>
-    <div class="col-10">
+    <div class="row mt-5">
+      <div class="col-1"></div>
+      <div class="col-10">
 
-      <center> <a><button class="btn btn-light addhlive">เพิ่มชื่อสามัญ</button></a>&nbsp;&nbsp;<label>ค้นหา :&nbsp;&nbsp;</label><input type="text" name="annimal_name_search" class="form">&nbsp;&nbsp;<input type="submit" class="btn btn-light" value="ค้นหา"></center>
+        <center> <a><button class="btn btn-light addhlive">เพิ่มชื่อสามัญ</button></a>&nbsp;&nbsp;<label>ค้นหา :&nbsp;&nbsp;</label><input type="text" name="annimal_name_search" class="form">&nbsp;&nbsp;<input type="submit" class="btn btn-light" value="ค้นหา"></center>
 
-      <p class="mt-5">ค้นหาสัตว์กรณีแก้ไข</p>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">ลำดับที่</th>
-            <th scope="col">ชื่อสามัญไทย</th>
-            <th scope="col">ชื่อสามัญภาษาอังกฤษ</th>
-            <th scope="col">วันรับเข้า</th>
-            <th scope="col">จำนวนมีชีวิต</th>
-            <th scope="col">เพิ่มสัตว์มีชีวิต</th>
-            <th scope="col">จำนวนตาย</th>
-            <th scope="col">เพิ่มสัตว์ตาย</th>
-            <th scope="col">ลบ</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>1</th>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td><a href="addehave.php" class="btn btn-light addhlive" href="#"><img src="picture/plus.png" width="25px" height="20px"></a></td>
-            <td>-</td>
-            <td><a href="addedead.php" class="btn btn-light addhlive" href="#"><img src="picture/plus.png" width="25px" height="20px"></a></td>
-            <td><button class="btn btn-light deleteannimal"><img src="picture/delete.png" width="20px" height="20px"></button></td>
-          </tr>
-        </tbody>
-      </table>
-
-    </div>
-    <div class="col-1"></div>
-  </div>
-
-  <button class="btn btn-light float-left back">ย้อนกลับ</button>
-  <button class='btn btn-light float-right'>รายงานการนำไปใช้ประโชยน์<br>ของสัตว์กรณีแก้ไข</button>
-
-</div>
-<div class="col-1 p-5">
-</div>
-
-<!-- modal -->
-<div class="modal fade" id="addhlive" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content p-3">
-      <div class="modal-header">
-        <h5 class="modal-title float-left" id="exampleModalCenterTitle">เพิ่มชื่อสามัญ</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
+        <p class="mt-5">ค้นหาสัตว์กรณีแก้ไข</p>
         <table class="table">
           <thead>
             <tr>
-              <th>ลำดับ</th>
-              <th>ชื่อสามัญไทย</th>
-              <th>ชื่อสามัญอังกฤษ</th>
-              <th>เลือก</th>
+              <th scope="col">ลำดับที่</th>
+              <th scope="col">ชื่อสามัญไทย</th>
+              <th scope="col">ชื่อสามัญภาษาอังกฤษ</th>
+              <th scope="col">วันรับเข้า</th>
+              <th scope="col">จำนวนมีชีวิต</th>
+              <th scope="col">เพิ่มสัตว์มีชีวิต</th>
+              <th scope="col">จำนวนตาย</th>
+              <th scope="col">เพิ่มสัตว์ตาย</th>
+              <th scope="col">ลบ</th>
             </tr>
-
           </thead>
           <tbody>
             <tr>
-              <td>1</td>
-              <td>เสือกำ</td>
-              <td>blacktiger</td>
-              <td><input type="checkbox" name="เสือดำ" value=""></td>
+              <th>1</th>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td><a href="addehave.php" class="btn btn-light addhlive" href="#"><img src="picture/plus.png" width="25px" height="20px"></a></td>
+              <td>-</td>
+              <td><a href="addedead.php" class="btn btn-light addhlive" href="#"><img src="picture/plus.png" width="25px" height="20px"></a></td>
+              <td><button class="btn btn-light deleteannimal"><img src="picture/delete.png" width="20px" height="20px"></button></td>
             </tr>
           </tbody>
-
         </table>
+
       </div>
-      <div class="mt-2">
-        <button type="button" class="btn btn-light float-right">เพิ่มลงในตาราง</button>
-      </div>
+      <div class="col-1"></div>
     </div>
+
+    <button class="btn btn-light float-left back">ย้อนกลับ</button>
+    <button class='btn btn-light float-right'>รายงานการนำไปใช้ประโชยน์<br>ของสัตว์กรณีแก้ไข</button>
+
   </div>
-</div>
-
-
-
-
-
-<div class="modal fade" id="showeditannimal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content p-3">
-      <div class="modal-header">
-        <h5 class="modal-title float-left" id="exampleModalCenterTitle">แก้ไขชนิดสัตว์</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <input type="text" name="nameannimal_edit" class="form-control">
-        </form>
-      </div>
-      <div class="mt-2">
-        <center><button type="button" class="btn btn-light">บันทึก</button></center>
-      </div>
-    </div>
+  <div class="col-1 p-5">
   </div>
-</div>
 
-<div class="modal fade" id="deleteannimal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content p-3">
-      <div class="modal-header">
-        <h5 class="modal-title float-left" id="exampleModalCenterTitle">ลบ</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <center><img src="picture/unnamed.png" width="100px" height="100px">
-          <h1>ต้องการลบ<br>ข้อมูลหรือไม่</h1></center>
+  <!-- modal -->
+  <div class="modal fade" id="addhlive" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content p-3">
+        <div class="modal-header">
+          <h5 class="modal-title float-left" id="exampleModalCenterTitle">เพิ่มชื่อสามัญ</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form action="code/mannageannimaledit/addanimal.php" method="POST">
+          <div class="modal-body">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>ลำดับ</th>
+                  <th>ชื่อสามัญไทย</th>
+                  <th>ชื่อสามัญอังกฤษ</th>
+                  <th>เลือก</th>
+                </tr>
+
+              </thead>
+              <tbody>
+                <?php 
+                $i = 1;
+                foreach ($listanimal as $key => $value) { 
+                  ?>
+                  <tr>
+                    <td><?php echo $i; ?></td>
+                    <td><?php echo $value['Thai_Common_Name'];?></td>
+                    <td><?php echo $value['English_Common_Name'];?></td>
+                    <td><input type="checkbox" name="Animal_ID[]" value="<?php echo $value['Animal_ID'];?>"></td>
+                  </tr>
+                  <?php $i++; } ?>
+            </tbody>
+
+          </table>
         </div>
         <div class="mt-2">
-          <button type="button" class="btn btn-light float-left">ยืนยัน</button><button type="button" class="btn btn-light float-right" data-dismiss="modal" aria-label="Close">ยกเลิก</button>
+          <button type="submit" class="btn btn-light float-right">เพิ่มลงในตาราง</button>
+        </div>
+        </form>
         </div>
       </div>
     </div>
-  </div>
 
 
 
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  <script type="text/javascript">
 
 
-     $(document).ready(function() {
+    <div class="modal fade" id="showeditannimal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content p-3">
+          <div class="modal-header">
+            <h5 class="modal-title float-left" id="exampleModalCenterTitle">แก้ไขชนิดสัตว์</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <input type="text" name="nameannimal_edit" class="form-control">
+            </form>
+          </div>
+          <div class="mt-2">
+            <center><button type="button" class="btn btn-light">บันทึก</button></center>
+          </div>
+        </div>
+      </div>
+    </div>
 
-      $('.back').on('click', function (e) {
-              e.preventDefault()
-              window.location.replace("http://localhost/animal_manager/mannageuser.php");
-
-            })
-          $('#myTab a').on('click', function (e) {
-            e.preventDefault()
-            $(this).tab('show')
-          })
-
-          $('.deleteannimal').on('click', function () {
-            $('#deleteannimal').modal('show');
-          })
-
-          $('.addhlive').on('click', function () {
-            $('#addhlive').modal('show');
-          })
-
-           $('.showeditannimal').on('click', function () {
-            $('#showeditannimal').modal('show');
-          })
-
-          $('.mnl').click(function(event) {
-
-            var page = 0;
-            var id = $(this).data('id');
-            var dism = document.getElementById("display-m");
-            var dis1 = document.getElementById("display-1");
-            var dis2 = document.getElementById("display-2");
+    <div class="modal fade" id="deleteannimal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content p-3">
+          <div class="modal-header">
+            <h5 class="modal-title float-left" id="exampleModalCenterTitle">ลบ</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <center><img src="picture/unnamed.png" width="100px" height="100px">
+              <h1>ต้องการลบ<br>ข้อมูลหรือไม่</h1></center>
+            </div>
+            <div class="mt-2">
+              <button type="button" class="btn btn-light float-left">ยืนยัน</button><button type="button" class="btn btn-light float-right" data-dismiss="modal" aria-label="Close">ยกเลิก</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
 
-            if (id==1) {
-              dis1.style.display = 'block';
-              dism.style.display = 'none';
-              dis2.style.display = 'none';
-              page == 1 ;
-            }
-            if (id==2) {
-              dism.style.display = 'none';
-              dis1.style.display = 'none';
-              dis2.style.display = 'block';
-            }
-            else{
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+      <script type="text/javascript">
 
-            }
 
-          });
+       $(document).ready(function() {
+
+        $('.back').on('click', function (e) {
+          e.preventDefault()
+          window.location.replace("http://localhost/animal_manager/mannageuser.php");
+
+        })
+        $('#myTab a').on('click', function (e) {
+          e.preventDefault()
+          $(this).tab('show')
+        })
+
+        $('.deleteannimal').on('click', function () {
+          $('#deleteannimal').modal('show');
+        })
+
+        $('.addhlive').on('click', function () {
+          $('#addhlive').modal('show');
+        })
+
+        $('.showeditannimal').on('click', function () {
+          $('#showeditannimal').modal('show');
+        })
+
+        $('.mnl').click(function(event) {
+
+          var page = 0;
+          var id = $(this).data('id');
+          var dism = document.getElementById("display-m");
+          var dis1 = document.getElementById("display-1");
+          var dis2 = document.getElementById("display-2");
+
+
+
+          if (id==1) {
+            dis1.style.display = 'block';
+            dism.style.display = 'none';
+            dis2.style.display = 'none';
+            page == 1 ;
+          }
+          if (id==2) {
+            dism.style.display = 'none';
+            dis1.style.display = 'none';
+            dis2.style.display = 'block';
+          }
+          else{
+
+          }
+
         });
+      });
 
-  </script>
-</body>
-</html>
+    </script>
+  </body>
+  </html>
